76b2b89db8357b2c6a73dd9c2271138c6a968554
Russ Lightsey <texanruss3@gmail.com>
2021-08-19 19:37:46 -0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('world', 'russ3', null, {
+const sequelize = new Sequelize('databaseName', 'user', 'password', {
   host: 'localhost',
   port: 5432,
   dialect: 'postgres',
-  logging: false,
+  logging: false, //change to true if you would like to see the raw queries in console
 });
 
 module.exports = sequelize;
